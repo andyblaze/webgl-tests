693c1aecd1915b38e85793d18e8313df51629760
andyblaze <andy@mediagraphic.co.uk>
2026-05-29 13:40:48 +0100

    snowstorm

--- snowstorm/js/perlin.js
+++ snowstorm/js/perlin.js
@@ -1,69 +1,40 @@
 export default class Perlin {
-
-    constructor(seed = 1) {
+    constructor(seed= ) {
 
         this.seed = seed;
-
-        //
         // cache generated lattice values
-        //
         this.values = {};
     }
-
-    //
     // --------------------------------------------------
     // PUBLIC
     // --------------------------------------------------
-    //
-
     sample(x) {
-
-        //
         // surrounding integer positions
-        //
         const x0 = Math.floor(x);
         const x1 = x0 + 1;
 
-        //
         // interpolation amount
-        //
         const t = x - x0;
 
-        //
         // smooth interpolation curve
-        //
         const fadeT = this.fade(t);
 
-        //
         // deterministic random values
-        //
         const v0 = this.randomValue(x0);
         const v1 = this.randomValue(x1);
 
-        //
         // interpolate
-        //
         return this.lerp(v0, v1, fadeT);
     }
-
-    //
     // --------------------------------------------------
     // INTERNALS
     // --------------------------------------------------
-    //
-
     randomValue(x) {
-
-        //
         // cached?
-        //
         if (this.values[x] !== undefined) {
             return this.values[x];
         }
-
-        //
         // deterministic pseudo-random
-        //
         let n = x;
 
         n = (n << 13) ^ n;
@@ -80,20 +51,14 @@ export default class Perlin {
                 ) & 0x7fffffff
             ) / 1073741824.0;
 
-        //
-        // normalize:
-        // -1..1  ->  0..1
-        //
+        // normalize: -1..1  ->  0..1
         const normalized = (result + 1) * 0.5;
 
         this.values[x] = normalized;
 
         return normalized;
     }
-
-    //
     // classic Perlin fade curve
-    //
     fade(t) {
 
         return (
@@ -101,10 +66,7 @@ export default class Perlin {
             (t * (t * 6 - 15) + 10)
         );
     }
-
-    //
     // linear interpolation
-    //
     lerp(a, b, t) {
 
         return a + (b - a) * t;
