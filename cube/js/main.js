880bc1e0ab477bde970b94602731943a835e8178
andyblaze <andy@mediagraphic.co.uk>
2026-06-09 11:19:10 +0100

    cube

--- cube/js/main.js
+++ cube/js/main.js
@@ -5,14 +5,36 @@ const scale = document.getElementById("scale");
 const x = document.getElementById("x");
 const y = document.getElementById("y");
 const rot = document.getElementById("rot");
+const scaleLbl = document.getElementById(scale.dataset.lbl);
+const xLbl = document.getElementById(x.dataset.lbl);
+const yLbl = document.getElementById(y.dataset.lbl);
+
 
 function update() {
+const srcImg = document.getElementById("srcImg");
+const natW = srcImg.naturalWidth;
+const natH = srcImg.naturalHeight;
+const cliW = srcImg.clientWidth;
+const cliH = srcImg.clientHeight;
+const imgScaleX = (natW / cliW).toFixed(2);
+const imgScaleY = (natH / cliH).toFixed(2);
+
+
+const scales = document.getElementById("scales");
+
+
     const s = parseFloat(scale.value);
     const px = parseInt(x.value);
     const py = parseInt(y.value);
     const r = parseInt(rot.value) * 90;
     const w = s * 300;
     const fs = 100 * s;
+
+    const trueX = Math.round(px * (natW / cliW));
+    const trueY = Math.round(py * (natH / cliH));
+    const trueFs = Math.round(fs * (natW / cliW));
+
+scales.innerHTML = `Nat W: ${natW} , NatH: ${natH} , CliW: ${cliW} , CliH: ${cliH} , <br> ImgScaleX: ${imgScaleX} , ImgScaleY: ${imgScaleY} , True X: ${trueX} , True Y: ${trueY} , True Face Sz: ${trueFs}`;
 
     net.style.left = `${px}px`;
     net.style.top = `${py}px`;
@@ -21,6 +43,9 @@ function update() {
           f.style.width = `${fs}px`; 
         f.style.height = `${fs}px`;     
     }
+    scaleLbl.innerText = s;
+    xLbl.innerText = px;
+    yLbl.innerText = py;
 }
 
 scale.oninput = update;
