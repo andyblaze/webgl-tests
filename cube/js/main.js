c7f9b3c667c1562cbbd72ef8dc962806fb2ef06c
andyblaze <andy@mediagraphic.co.uk>
2026-06-09 13:51:23 +0100

    cube

--- cube/js/main.js
+++ cube/js/main.js
@@ -1,4 +1,5 @@
 const net = document.getElementById("net");
+
 const faces = document.getElementsByClassName("face");
 
 const scale = document.getElementById("scale");
@@ -19,6 +20,8 @@ const cliH = srcImg.clientHeight;
 const imgScaleX = (natW / cliW).toFixed(2);
 const imgScaleY = (natH / cliH).toFixed(2);
 
+//net.style.maxWidth = `${cliW}px`;
+
 
 const scales = document.getElementById("scales");
 
@@ -33,14 +36,15 @@ const scales = document.getElementById("scales");
     const trueX = Math.round(px * (natW / cliW));
     const trueY = Math.round(py * (natH / cliH));
     const trueFs = Math.round(fs * (natW / cliW));
+    const trueNetW = Math.round(w * (natW / cliW));
 
-scales.innerHTML = `Nat W: ${natW} , NatH: ${natH} , CliW: ${cliW} , CliH: ${cliH} , <br> ImgScaleX: ${imgScaleX} , ImgScaleY: ${imgScaleY} , True X: ${trueX} , True Y: ${trueY} , True Face Sz: ${trueFs}`;
+scales.innerHTML = `Nat W: ${natW} , NatH: ${natH} , CliW: ${cliW} , CliH: ${cliH} , <br> ImgScaleX: ${imgScaleX} , ImgScaleY: ${imgScaleY} , <br> True X: ${trueX} , True Y: ${trueY} , True Face Sz: ${trueFs} , <br> Net W: ${w}, True NetW: ${trueNetW}`;
 
     net.style.left = `${px}px`;
-    net.style.top = `${py}px`;
-    net.style.width = `${w}px`;
+    net.style.top = `${py}px`; //
+    net.style.width = `${w}px`; 
     for ( const f of faces ) {
-          f.style.width = `${fs}px`; 
+        f.style.width = `${fs}px`; 
         f.style.height = `${fs}px`;     
     }
     scaleLbl.innerText = s;
