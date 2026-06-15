751f59d1c32b44761a79f3c88b264a2af441a0f7
andyblaze <andy@mediagraphic.co.uk>
2026-06-15 15:44:56 +0100

    ocean

--- ocean-wave/js/gravity-well.js
+++ ocean-wave/js/gravity-well.js
@@ -5,9 +5,9 @@ export default class GravityWell {
         this.x = mt_rand(5, 20);
         this.y = mt_rand(5, 20);
         this.z = 0;
-        this.strength = Math.random() < 0.5 ? mt_rand(10, 13) : -mt_rand(10, 13);
-        this.spread = mt_rand(30, 50);
-        this.orbitSpeed = mt_rand(7000, 12000);
+        this.strength = Math.random() < 0.5 ? mt_rand(15, 18) : -mt_rand(15, 18);
+        this.spread = mt_rand(30, 60);
+        this.orbitSpeed = mt_rand(6000, 9000);
         this.orbitRadius = mt_rand(25, 60);  
         this.phase = Math.random() * Math.PI * 2;
     }
@@ -17,8 +17,16 @@ export default class GravityWell {
         const distanceSquared = (dx * dx) + (dy * dy);
         return this.strength * Math.exp(-distanceSquared / this.spread);
     }
-    update(t) {
-        //this.strength = this.strength * Math.sin(t * this.orbitSpeed + this.orbitRadius);
+update(t) {
+    const angle = (t / this.orbitSpeed) + this.phase;
+
+    const xRadius = this.orbitRadius * 2.10;
+    const yRadius = this.orbitRadius * 1.0;
+
+    this.x = Math.cos(angle) * xRadius;
+    this.y = Math.sin(angle) * yRadius;
+}    
+    updateo(t) {
         const angle = (t / this.orbitSpeed) + this.phase;
         this.x = Math.cos(angle) * this.orbitRadius;
         this.y = Math.sin(angle) * this.orbitRadius;
