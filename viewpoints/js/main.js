60c04e9e2a06c7214ace3e84242aba22da8eb02d
andyblaze <andy@mediagraphic.co.uk>
2026-06-28 14:38:01 +0100

    views

--- viewpoints/js/main.js
+++ viewpoints/js/main.js
@@ -14,14 +14,10 @@ document.body.appendChild(renderer.domElement);
 //-----------------------------------------------------
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x202530);
+scene.background = new THREE.Color(0xc5c5c5);
+scene.fog = new THREE.Fog(0xa0a5a0, 6, 100);
 
-const mainCamera = new THREE.PerspectiveCamera(
-45,
-innerWidth/innerHeight,
-0.1,
-100
-);
+const mainCamera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 100);
 
 mainCamera.position.set(8,5,10);
 
@@ -56,100 +52,84 @@ scene.add(light);
 // Floor
 //-----------------------------------------------------
 
-const grid = new THREE.GridHelper(20,20);
-scene.add(grid);
+const floor = new THREE.Mesh(
+
+    new THREE.PlaneGeometry(16, 16),
+
+    new THREE.MeshStandardMaterial({
+        color: 0x550000,
+        roughness: 0.95,
+        metalness: 0.05
+    })
+
+);
+
+floor.rotation.x = -Math.PI / 2;
+floor.receiveShadow = true;
+
+scene.add(floor);
 
 //-----------------------------------------------------
 // Objects
 //-----------------------------------------------------
 
 const torus = new THREE.Mesh(
-new THREE.TorusKnotGeometry(1,0.35,128,16),
-new THREE.MeshStandardMaterial({
-color:0x55ccff,
-metalness:0.3,
-roughness:0.2
-})
+    new THREE.TorusKnotGeometry(1,0.35,128,16),
+    new THREE.MeshStandardMaterial({
+        color:0x55ccff,
+        metalness:0.3,
+        roughness:0.2
+    })
 );
 
 torus.position.y=2;
 scene.add(torus);
 
 const sphere = new THREE.Mesh(
-new THREE.SphereGeometry(0.35),
-new THREE.MeshStandardMaterial({
-color:"orange",
-emissive:"orange",
-emissiveIntensity:0.3
-})
+    new THREE.TorusGeometry(0.35, 0.1),
+    new THREE.MeshStandardMaterial({
+        color:"orange",
+        emissive:"orange",
+        emissiveIntensity:0.3
+    })
 );
 
 scene.add(sphere);
 
-//-----------------------------------------------------
-
-window.addEventListener("resize",()=>{
-
-mainCamera.aspect=innerWidth/innerHeight;
-mainCamera.updateProjectionMatrix();
-
-renderer.setSize(innerWidth,innerHeight);
-
-});
-
-//-----------------------------------------------------
-
 const clock = new THREE.Clock();
-
-
-//monitor.position.z += 0.05;
-//frame.position.z += 0.04;
-
 const monitorCam = monitor.camera;
-function animate(){
 
-requestAnimationFrame(animate);
+function animate() {
+    const t = clock.getElapsedTime();
 
-const t = clock.getElapsedTime();
+    torus.rotation.x = t * 0.5;
+    torus.rotation.y = t * 0.2;
+    torus.rotation.z = t * 0.1;
 
-//animate scene
+    sphere.position.set(Math.cos(t) * 3, 1.5 + Math.sin(t * 2) * 0.5, Math.sin(t) * 3);
+    sphere.rotation.x = t * 0.73;
+    sphere.rotation.y = t * 0.91;
+    //sphere.rotation.z = t * 0.17;
 
-torus.rotation.x=t*0.5;
-torus.rotation.y=t*0.2;
-torus.rotation.z=t*0.1;
+    monitor.toggleVisibility();
 
-sphere.position.set(
-Math.cos(t)*3,
-1.5+Math.sin(t*2)*0.5,
-Math.sin(t)*3
-);
+    renderer.setRenderTarget(renderTarget);
+    renderer.render(scene, monitorCam);
+    renderer.setRenderTarget(null);
 
-//==================================================
-// FIRST
-// Render scene into texture
-//==================================================
+    monitor.toggleVisibility();
 
-//renderer.setRenderTarget(renderTarget);
-//renderer.render(scene,monitorCamera);
+    renderer.render(scene, mainCamera);
 
-//==================================================
-// SECOND
-// Render normal scene
-//==================================================
-
-monitor.toggleVisibility();
-//frame.visible = false;
-
-renderer.setRenderTarget(renderTarget);
-renderer.render(scene, monitorCam);
-
-renderer.setRenderTarget(null);
-
-monitor.toggleVisibility();
-//frame.visible = true;
-
-renderer.render(scene, mainCamera);
-
+    requestAnimationFrame(animate);
 }
 
 animate();
+
+//-----------------------------------------------------
+
+window.addEventListener("resize",()=>{
+    mainCamera.aspect=innerWidth/innerHeight;
+    mainCamera.updateProjectionMatrix();
+    renderer.setSize(innerWidth,innerHeight);
+});
