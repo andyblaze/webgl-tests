508f6ec59ac8aefdadd386e551f84aa9eca2e5fe
andyblaze <andy@mediagraphic.co.uk>
2026-07-23 12:23:12 +0100

    march cubes

--- marching-cubes/js/main.js
+++ marching-cubes/js/main.js
@@ -3,6 +3,7 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
+import { MarchingCubes } from "three/addons/objects/MarchingCubes.js";
 import { mt_rand } from "./functions.js";
 
 
@@ -14,8 +15,8 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 100);
-camera.position.set(0, 1.5, 8);
+const camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 0.1, 100);
+camera.position.set(0, 0, 8);
 
 new OrbitControls(camera, renderer.domElement);
 
@@ -24,6 +25,28 @@ scene.add(new THREE.AmbientLight(0xffffff, 2));
 const dl = new THREE.DirectionalLight(0xc6c6c6, 1);
 dl.position.set(0, 10, 12);
 scene.add(dl);
+
+const material = new THREE.MeshPhysicalMaterial({
+    color: 0x44aaff,
+    roughness: 0.25,
+    metalness: 0.03,
+    transmission: 0.2,
+    clearcoat: 1
+});
+
+const resolution = 64;
+
+const metaball = new MarchingCubes(
+    resolution,
+    material,
+    true,
+    true
+);
+
+metaball.position.set(0, 0, 0);
+metaball.scale.set(3, 3, 3);
+
+scene.add(metaball);
 
 const composer = new EffectComposer(renderer);
 composer.addPass(new RenderPass(scene, camera));
@@ -34,7 +57,47 @@ composer.addPass(
 const clock = new THREE.Clock();
 function animate() {
     const elapsed = clock.getElapsedTime();
-    // three objects rendering goes here
+    metaball.reset();
+
+const t = elapsed;
+
+metaball.reset();
+
+metaball.addBall(
+    0.35 + Math.sin(t) * 0.15,
+    0.5,// + Math.sin(t) * 0.15,
+    0.5,
+    0.5,
+    20,
+    5
+);
+
+metaball.addBall(
+    0.35 + Math.cos(t * 1.3) * 0.15,
+    0.5,
+    0.5,
+    0.5,
+    20,
+    5
+);
+/*
+metaball.addBall(
+    0.5,
+    0.5 + Math.sin(t * 0.8) * 0.15,
+    0.5,
+    0.5,
+    12
+);
+
+metaball.addBall(
+    0.5,
+    0.5,
+    0.5 + Math.sin(t * 0.8) * 0.215,
+    0.5,
+    12
+);
+*/
+metaball.update();
     composer.render();
     requestAnimationFrame(animate);
 }
