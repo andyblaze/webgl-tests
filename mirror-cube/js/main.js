5e509f3051bb4b93e9853672d44ef1c7c2b3986f
andyblaze <andy@mediagraphic.co.uk>
2026-07-25 12:14:21 +0100

    mirror cube

--- mirror-cube/js/main.js
+++ mirror-cube/js/main.js
@@ -71,148 +71,162 @@ rimLight.position.set(
 
 scene.add(areaLight);
 
-const loader = new THREE.TextureLoader();
+const cubeSize = 6;
+//const cubeRadius = Math.sqrt(3) * cubeSize * 0.5;
+const cubeCentre = new THREE.Vector3(0, 0, -8);
 
-const canvasTexture = loader.load("canvas.png");
-const normalTexture = loader.load("normal.png");
+class Cube {
+    constructor(three, cubeSize, cubeCentre, scene) {
+        this.visible = true;
+        const loader = new THREE.TextureLoader();
+        const canvasTexture = loader.load("canvas.png");
+        const normalTexture = loader.load("normal.png");
 
-const cube = new THREE.Mesh(
-    new RoundedBoxGeometry(5,5,5, 6, 0.08),
-    new THREE.MeshPhysicalMaterial({
+        this.cube = new three.Mesh(
+            new RoundedBoxGeometry(cubeSize, cubeSize, cubeSize, 6, 0.08),
+            new three.MeshPhysicalMaterial({
+                color: 0x444444,
+                transparent: true,
+                opacity: 1,
 
-        color: 0x444444,
-        transparent: true,
-        opacity: 1,
+                metalness: 1,
+                roughness: 0.08,
 
-        metalness: 1,
-        roughness: 0.08,
+                clearcoat: 1,
+                clearcoatRoughness: 0.05,
 
-        clearcoat: 1,
-        clearcoatRoughness: 0.05,
+                envMap: cubeRenderTarget.texture,
+                envMapIntensity: 8,
 
-        envMap: cubeRenderTarget.texture,
-        envMapIntensity: 8,
+                reflectivity: 1,
+                ior: 1.5,
 
-        reflectivity: 1,
-        ior: 1.5,
+                normalMap: normalTexture,
+                normalScale: new three.Vector2(0.15, 0.15)
 
-        normalMap: normalTexture,
-        normalScale: new THREE.Vector2(0.15, 0.15)
+            })
+        );
+        this.cube.position.copy(cubeCentre);
+        scene.add(this.cube);
+        const shellSize = cubeSize * 1.0125;
 
-    })
-);
-cube.position.set(0,0,-8);
-scene.add(cube);
-rimLight.lookAt(cube.position);
+        const geometry = new RoundedBoxGeometry(shellSize, shellSize, shellSize, 6, 0.08);
+
+        this.shell = new three.Mesh(
+            geometry,
+            new three.MeshBasicMaterial({
+                color:0xffffff,
+                transparent:true,
+                opacity:0.05,
+                ior: 1.5
+            })
+        );
+
+        this.shell.position.copy(this.cube.position);
+        scene.add(this.shell);
+    }
+    update(elapsed) {
+        this.cube.rotation.x = elapsed * 0.07;
+        this.cube.rotation.y = elapsed * 0.09;
+        this.cube.rotation.z = elapsed * 0.08;
+        this.shell.rotation.x = elapsed * 0.07;
+        this.shell.rotation.y = elapsed * 0.09;
+        this.shell.rotation.z = elapsed * 0.08;
+    }
+    getPosition() {
+        return this.cube.position;
+    }
+    toggleVisibilty() {
+        this.cube.visible = !this.cube.visible;
+    }
+}
+const cube = new Cube(THREE, cubeSize, cubeCentre, scene);
+rimLight.lookAt(cube.getPosition());
 
 scene.add(rimLight);
 
-const geometry = new RoundedBoxGeometry(
-    5.05,
-    5.05,
-    5.05,
-    6,
-    0.08
-);
-
-const shell = new THREE.Mesh(
-    geometry,//new THREE.BoxGeometry(4.05,4.05,4.05),
-    new THREE.MeshBasicMaterial({
-        color:0xffffff,
-        transparent:true,
-        opacity:0.05,
-        ior: 1.5
-    })
-);
-
-shell.position.copy(cube.position);
-scene.add(shell);
-
-/*const grp = new THREE.Group();
-grp.add(cube, shell);
-grp.position.set(0,0,-8);*/
-
-const sphereCount = 100;
-
-const sphereGeometry = new THREE.SphereGeometry(
-    0.125,
-    32,
-    32
-);
-
-const sphereMaterial = new THREE.MeshStandardMaterial({
-    color: 0xff2244,
-    roughness: 0.25,
-    metalness: 0.1
-});
-
-const spheres = [];
-
-const cubeBounds = {
-    x: 2,
-    y: 2,
-    zFront: -6,   // front face of cube
-    zBack: -10
-};
-
-const sphereRadius = 0.125;
 
 
-for (let i = 0; i < sphereCount; i++) {
+class Sphere {
+    constructor(three, pos) {
+        this.sphereRadius = 0.125;
+        this.sphereGeometry = new three.SphereGeometry(this.sphereRadius, 32, 32);
+        this.sphereMaterial = new three.MeshStandardMaterial({
+            color: 0xff2244,
+            roughness: 0.25,
+            metalness: 0.1
+        }); 
 
-    const sphere = new THREE.Mesh(
-        sphereGeometry,
-        sphereMaterial.clone()
-    );
-
-    sphere.position.set(
-        THREE.MathUtils.randFloat(-6, 6),
-        THREE.MathUtils.randFloat(-3, 3),
-        THREE.MathUtils.randFloat(-2, -12)
-    );
-
-
-    // if sphere would intersect cube volume, move it in front
-    const intersectsCube =
-        Math.abs(sphere.position.x) < cubeBounds.x + sphereRadius &&
-        Math.abs(sphere.position.y) < cubeBounds.y + sphereRadius &&
-        sphere.position.z < cubeBounds.zFront + sphereRadius &&
-        sphere.position.z > cubeBounds.zBack - sphereRadius;
-
-
-    if (intersectsCube) {
-        sphere.position.z = cubeBounds.zFront + sphereRadius + 0.5;
-    }
-
-
-    sphere.material.color.setHSL(
-        Math.random(),
-        0.8,
-        0.5
-    );
-    if ( Math.random() < 0.1 ) {
-        sphere.material.emissive = sphere.material.color;
-        sphere.material.emissiveIntensity = 2;
-    }
-    scene.add(sphere);
-    spheres.push(sphere);
-
-    if ( Math.random() < -0.1 ) {
-        const glow = new THREE.PointLight(
-            0xff2244,
-            3,
-            4
+        this.sphere = new three.Mesh(
+            this.sphereGeometry,
+            this.sphereMaterial.clone()
         );
 
-        glow.position.copy(sphere.position);
-        scene.add(glow);
+        this.sphere.position.copy(pos);
+        this.sphere.material.color.setHSL(Math.random(), 0.8, 0.5);
+    }
+    get mesh() {
+        return this.sphere
+    }
+    update(p) {
+        this.sphere.position.y += p;//Math.sin(t) * 0.002;
     }
 }
+class SphereManager {
+    constructor(three) {
+        this.sphereRadius = 0.125;
+        this.sphereCount = 100;
+        this.spheres = [];
+
+        this.sphereGeometry = new three.SphereGeometry(this.sphereRadius, 32, 32);
+        this.sphereMaterial = new three.MeshStandardMaterial({
+            color: 0xff2244,
+            roughness: 0.25,
+            metalness: 0.1
+        });        
+    }
+    createSpheres(three, cubeSize, cubeCentre, scene) {
+        const cubeRadius = Math.sqrt(3) * cubeSize * 0.5;
+        const exclusionRadius = cubeRadius + this.sphereRadius + 0.25;
+
+        for ( let i = 0; i < this.sphereCount; i++ ) {
+            const pos = this.getPosition(three, cubeCentre, exclusionRadius);
+            const sphere = new Sphere(three, pos);
+
+            scene.add(sphere.mesh);        
+
+            this.spheres.push(sphere);
+        }
+    }
+    getPosition(three, cubeCentre, exclusionRadius) {
+        const p = new three.Vector3();
+        do {
+            p.set(
+                three.MathUtils.randFloat(-6, 6),
+                three.MathUtils.randFloat(-3, 3),
+                three.MathUtils.randFloat(-2, -10)
+            );
+        } while (
+            p.distanceTo(cubeCentre) < exclusionRadius
+        );
+        return p;
+    }
+    update(elapsed) {
+        this.spheres.forEach((sphere, i) => {
+            const t = elapsed * 0.5 + i;
+            const p = Math.sin(t) * 0.002;
+            sphere.update(p);
+        });
+    }
+}
+const sphereManager = new SphereManager(THREE);
+sphereManager.createSpheres(THREE, cubeSize, cubeCentre, scene);
+
 
 const composer = new EffectComposer(renderer);
 composer.addPass(new RenderPass(scene, camera));
 /*composer.addPass(
-    new UnrealBloomPass(new THREE.Vector2(innerWidth, innerHeight), 1.3, 0.5, 0.2)
+    new UnrealBloomPass(new THREE.Vector2(innerWidth, innerHeight), 0.6, 0.5, 0.2)
 );*/
 
 const clock = new THREE.Clock();
@@ -221,38 +235,20 @@ function animate(){
 
     const elapsed = clock.getElapsedTime();
 
-spheres.forEach((sphere, i) => {
+    sphereManager.update(elapsed);
 
-    const t = elapsed * 0.5 + i;
+    const pulse = (Math.sin(elapsed * 0.5) + 1) * 0.5;
+    areaLight.intensity = 3 + pulse * 2;
 
-const pulse = (Math.sin(elapsed * 0.5) + 1) * 0.5;
+    cube.update(elapsed);
 
-areaLight.intensity = 3 + pulse * 2;
+    // capture environment from cube position
+    cube.toggleVisibilty();
 
-    sphere.position.y += Math.sin(t) * 0.002;
-    sphere.rotation.x += 0.01;
-    sphere.rotation.y += 0.01;
+    cubeCamera.position.copy(cube.getPosition());
+    cubeCamera.update(renderer, scene);
 
-});
-
-cube.rotation.x = elapsed * 0.07;
-cube.rotation.y = elapsed * 0.09;
-cube.rotation.z = elapsed * 0.08;
-shell.rotation.x = elapsed * 0.07;
-shell.rotation.y = elapsed * 0.09;
-shell.rotation.z = elapsed * 0.08;
-
-
-// capture environment from cube position
-cube.visible = false;
-
-cubeCamera.position.copy(cube.position);
-cubeCamera.update(renderer, scene);
-
-cube.visible = true;
-
-
-composer.render();
+    cube.toggleVisibilty();
 
     composer.render();
 
