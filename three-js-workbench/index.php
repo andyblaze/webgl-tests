df12e9f0a5f911e9e72c48a59e94b6a2b30a9629
andyblaze <andy@mediagraphic.co.uk>
2026-08-01 09:33:56 +0100

    three workbench

--- three-js-workbench/index.php
+++ three-js-workbench/index.php
@@ -2,17 +2,31 @@
 include('php/utils.php');
 
 $ctrlsCfg = json_decode(file_get_contents('ctrls-config.json'));
-$ctrls = '';
+$ctrls = $lights = '';
 foreach ( $ctrlsCfg->material as $key=>$val ) {
     if ( $val->ctrl === "range" )
         $ctrls .= slider($key, $val, 'material');
     if ( $val->ctrl === "color" )
         $ctrls .= colorPicker($key, $val, 'material');
 }
+ /*   "lights": [
+        {"type": "ambient", "color": "#ffffff", "intensity": 2},
+        {"type": "directional", "color": "#c60000", "intensity": 2, "pos": { "x": 0, "y": 8, "z": 0 }},
+        {"type": "directional", "color": "#00c600", "intensity": 2, "pos": { "x": -8, "y": 8, "z": 7 }}
+    ],*/
+foreach ( $ctrlsCfg->lights as $key=>$light ) {
+    $name = $light->type . $key;
+    $lbl = ucfirst(str_replace('-', ' ', $name));
+    $lights .= "<label>
+        {$lbl}: <span id=\"{$name}-lbl\">{$light->intensity}</span> 
+        <input type=\"range\" id=\"{$name}-slider\" min=\"0\" max=\"5\" step=\"0.5\" value=\"{$light->intensity}\" data-label=\"{$name}-lbl\" data-property=\"intensity\" data-type=\"{$light->type}\" data-index=\"lights\" autocomplete=\"off\" />
+    </label>";
+}
 
 $data = [
     'css' => link_tag('css/sys.css'),
-    'ctrls' => $ctrls
+    'ctrls' => $ctrls,
+    'lights' => $lights
 ];
 
 $html = file_get_contents('php/view.html');
